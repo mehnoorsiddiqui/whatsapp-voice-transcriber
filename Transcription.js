0b1d1c07d43239c0d5b494439cb403f644877f72
mehnoorsiddiqui <ayaankhan7532@gmail.com>
2023-03-30 20:32:47 +0500

    ref: fs.mkdtemp instead of tmp folder logic updated

--- Transcription.js
+++ Transcription.js
@@ -3,17 +3,32 @@ const {
   downloadAudio_,
 } = require("./services/WhatsAppCloudService");
 const transcribeAudio = require("./services/OpenAIWhisperService");
+const fs = require("fs").promises;
+const os = require("os");
 const path = require("path");
-const fs = require("fs/promises");
-const writeFile = fs.writeFile;
+
+//Create and remove temporary directory for function execution.
+const withTempFile = (fn) => withTempDir((dir) => fn(path.join(dir, "file")));
+
+const withTempDir = async (fn) => {
+  const tempDir = await fs.mkdtemp(
+    path.join(await fs.realpath(os.tmpdir()), path.sep)
+  );
+
+  try {
+    return await fn(tempDir);
+  } finally {
+    await fs.rm(tempDir, { recursive: true });
+  }
+};
 
 async function Transcription(to, audioID) {
   const downloadMedia = await downloadAudio_(audioID);
-  const fileBasePath = "./tmp";
-  const filePath = path.join(fileBasePath, `./${audioID}.ogg`);
-  await writeFile(filePath, await downloadMedia.buffer());
-  const text = await transcribeAudio(filePath);
-  await sendMessage_(to, text);
+  withTempFile(async (filePath) => {
+    await fs.writeFile(filePath, await downloadMedia.buffer());
+    const text = await transcribeAudio(filePath);
+    await sendMessage_(to, text);
+  });
 }
 
 module.exports = Transcription;
--- services/OpenAIWhisperService.js
+++ services/OpenAIWhisperService.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
 const { ApiController,ApiError,Client,FileWrapper} = require('openai-whisper-apilib');
-const fspromise = require("fs/promises");
-const unlink = fspromise.unlink;
 
 const client = new Client({
   timeout: 0,
@@ -11,28 +9,17 @@ const task = 'transcribe';
 const language = 'en';
 
 const transcribeAudio= async(audioFilePath)=>{
-    const audioFile = new FileWrapper(fs.createReadStream(`./${audioFilePath}`));
+    const audioFile = new FileWrapper(fs.createReadStream(`${audioFilePath}`));
     try {
-        const { result, ...httpResponse } = await apiController.transcribeFileAsrPost(audioFile, task, language);        
-        await deleteFile(audioFilePath);
+        const { result, ...httpResponse } = await apiController.transcribeFileAsrPost(audioFile, task, language);               
         return result.text;
       } catch(error) {
-        console.log(error)
-        await deleteFile(audioFilePath);
+        console.log(error)    
         if (error instanceof ApiError) {
           const errors = error.result;
           // const { statusCode, headers } = error;
         }
       }
-}
-
-async function deleteFile(filePath) {
-  try {
-    await unlink(filePath);
-    console.log(`Successfully deleted ${filePath}`);
-  } catch (err) {
-    console.error(err);
-  }
 }
 
 module.exports = transcribeAudio;
